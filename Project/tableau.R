17e2fe58d1350f54733f676f07c168d774aae421
Cristina Perez <chp2129@columbia.edu>
2017-09-01 17:42:17 -0400

    update

--- Project/tableau.R
+++ Project/tableau.R
@@ -1,4 +1,12 @@
+---
+title: "Tableau"
+author: "Cristina"
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+## Tableau
 
 <div class='tableauPlaceholder' id='viz1504299858794' style='position: relative'><noscript><a href='#'><img alt='Global Breastfeeding Collective ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ta&#47;Tables2&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='site_root' value='' /><param name='name' value='Tables2&#47;Dashboard1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ta&#47;Tables2&#47;Dashboard1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1504299858794');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='1404px';vizElement.style.height='869px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
-  
-  
